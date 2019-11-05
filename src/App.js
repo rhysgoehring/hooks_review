917a895e00a13d570cd5304db5914ec476260731
rhysgoehring <rhysgoehring@gmail.com>
2019-11-05 12:55:20 -0700

    created UserContext, added Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, createContext } from 'react';
 import Toggle from './Toggle';
 import useTitleInput from './hooks/useTitleInput';
+
+const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.add('new-fake_class')}>
-        Level Up Dishes
-      </h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: true
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add('new-fake_class')}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
