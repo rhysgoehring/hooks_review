2a76037243d4725ff01b4bcee3c09909e3f0950f
rhysgoehring <rhysgoehring@gmail.com>
2019-11-05 13:33:15 -0700

    created basic Counter component, rendering in App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect, useRef, createContext } from 'react';
 import Toggle from './Toggle';
+import Counter from './Counter';
 import useTitleInput from './hooks/useTitleInput';
-
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
   return (
-      <div className="main-wrapper" ref={ref}>
-        <h1 onClick={() => ref.current.classList.add('new-fake_class')}>
-          Level Up Dishes
-        </h1>
-        <Toggle />
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-          }}
-        >
-          <input
-            type="text"
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake_class')}>
+        Level Up Dishes
+      </h1>
+      <Toggle />
+      <Counter />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React, { userReducer } from 'react';
+
+const Counter = () => {
+  return (
+    <div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+};
+
+export default Counter;
