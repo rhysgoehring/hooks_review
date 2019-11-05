0a8a4ad73e4c84702f0f745f600874db7684ebba
rhysgoehring <rhysgoehring@gmail.com>
2019-11-05 11:21:38 -0700

    wrote function outside of component for handling form submission that performs an action and then sets input state back to empty

--- src/App.js
+++ src/App.js
@@ -8,10 +8,26 @@ const App = () => {
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <h3>{name}</h3>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log(`Email sent to ${value}`);
+  setValue('');
 };
 
 export default App;
