092ac404a338526973cc28331a26c8e1a22fc3a7
rhysgoehring <rhysgoehring@gmail.com>
2019-11-05 13:41:13 -0700

    added reducer and inital state to Counter, rendering count in h3

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,35 @@
-import React, { userReducer } from 'react';
+import React, { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return {
+        count: state.count + 1
+      };
+    case 'subtract':
+      return {
+        count: state.count - 1
+      };
+    case 'reset':
+      return {
+        count: initialState.count
+      };
+    default:
+      throw new Error();
+  }
+}
 
 const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
+      <h3>{state.count}</h3>
       <button>+</button>
       <button>-</button>
+      <button>Reset</button>
     </div>
   );
 };
