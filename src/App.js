052845a13a1172c39b3b0d451ace97cd593054ae
rhysgoehring <rhysgoehring@gmail.com>
2019-11-06 07:23:07 -0700

    removed code specific to the useMemo section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Toggle from './Toggle';
 import useTitleInput from './hooks/useTitleInput';
 
@@ -6,22 +6,9 @@ const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
-  const reverseWord = word => {
-    console.log('function called');
-    return word
-      .split('')
-      .reverse()
-      .join('');
-  };
-
-  const title = 'Level Up Dishes';
-
-  const TitleReversed = useMemo(() => reverseWord(name), [name]);
-
   return (
     <div className="main-wrapper" ref={ref}>
       <h1 onClick={() => ref.current.classList.add('new-fake_class')}>
-        {TitleReversed}
       </h1>
       <Toggle />
       <form
