31b3e19fe8267afb283a11f21287b4133087a0a8
rhysgoehring <rhysgoehring@gmail.com>
2019-11-08 08:11:57 -0700

    added cleanup function to useBodyScrollLock hook so when the component using the hook unmounts the user can scroll again

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -1,9 +1,20 @@
 import { useLayoutEffect } from 'react';
 
+// "Cleanup" can be thought of similarly to componentWillUnmount
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
     document.body.style.overflow = 'hidden';
+
+    // Cleanup:
+    // return function cleanup() {
+    //   document.body.style.overflow = '';
+    // };
+
+    // Cleanup w/ arrow function
+    return () => {
+      document.body.style.overflow = '';
+    };
   });
 }
 
